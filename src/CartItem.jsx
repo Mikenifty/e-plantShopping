677490f8300144d8794dc547b52442c8fe8151d8
Mikenifty <117452743+Mikenifty@users.noreply.github.com>
2024-10-10 18:32:49 +0800

    Completed the project

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -4,7 +4,7 @@ import { removeItem, updateQuantity } from './CartSlice';
 import './CartItem.css';
 import ProductList from './ProductList';
 
-const CartItem = ({ onContinueShopping }) => {
+const CartItem = ({ onContinueShopping, onHandleNotInCart }) => {
 
   const cart = useSelector(state => state.cart.items);
   const dispatch = useDispatch();
@@ -13,36 +13,53 @@ const CartItem = ({ onContinueShopping }) => {
   
   const calculateTotalAmount = () => {
     let totalCost = 0;
-        cart.map((item) => {
-        const stripCost = item.cost.replace(/[$,]/g,"");
-        const itemCost = stripCost * item.quantity;
-        console.log(itemCost);
-        totalCost += itemCost;
+        cart.map((item) => {                               // Initiate iteration
+        const stripCost = item.cost.replace(/[$,]/g,"");   // Remove the dollar sign from the price information
+        const itemCost = stripCost * item.quantity;        // Calculate the cost of an item by multiplying the quantity by the price
+        totalCost += itemCost;                             // Calculate the total cost for the cart by adding each item cost to the total
         });   
     return totalCost;  
   };
  
-  const handleContinueShopping = (e) => {
+  const handleContinueShopping = (e) => {  // Handle the continue shopping function
     return(onContinueShopping(e));
   };
 
-
+  const handleNotInCart = (item) => {       // Handle the styling of the add to cart button in the ProductList component
+    return(onHandleNotInCart(item));
+  };
 
   const handleIncrement = (item) => {
+     let newItem = structuredClone(item);   // Clone the state since it is read-only
+     newItem.quantity++;
+    dispatch(updateQuantity(newItem));      // Dispatch the updateQuantity action to update the quantity of items in the cart
   };
 
   const handleDecrement = (item) => {
-   
+    let newItem = structuredClone(item);    // Clone the state since it is read-only
+    if (newItem.quantity > 1) {             // Conditionally update the cloned cart
+      newItem.quantity--;
+      dispatch(updateQuantity(newItem));     // Dispatch the updateQuantity action to update the quantity of items in the cart
+    } else {
+      dispatch(removeItem(item));            // Dispatch the removeItem action to remove the item from the cart
+      handleNotInCart(item);                 // Handle the styling of the add to cart button in the ProductList component
+    }  
   };
 
   const handleRemove = (item) => {
+      dispatch(removeItem(item));             // Dispatch the removeItem action to remove the item from the cart
+      handleNotInCart(item);                  // Handle the styling of the add to cart button in the ProductList component
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+      const stripCost = item.cost.replace(/[$,]/g,"");  // Remove the dollar sign from the price information
+      let itemCost = stripCost * item.quantity;         // Calculate the cost of an item by multiplying the quantity by the price
+      return itemCost; 
   };
-  const handleCheckoutShopping = (e) => {
-        alert('Functionality to be added for future reference');
+
+  const handleCheckoutShopping = (e) => {        // Handle frustrated customers
+      alert('Sorry! Come again another day!');
   };
 
 
